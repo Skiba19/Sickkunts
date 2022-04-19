8ab624e137e32144f751bae224aa54484b6e2299
Skiba19 <skibagergo@gmail.com>
2022-04-19 02:24:20 +0200

    Bullets

---
+++ Sickkunts/Assets/Scripts/Bullet.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private Transform target;
+    private float speed=3f;
+    public GameObject impactEffect;
+    public void Seek(Transform _target)
+    {
+        target=_target;
+    }
+    void Update()
+    {
+        if(target==null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Debug.Log(target.name);
+        Vector3 dir=target.position-transform.position;
+        float distanceThisFrame=speed*Time.deltaTime;
+        if(dir.magnitude<=distanceThisFrame)
+        {
+            HitTarget();
+            return;
+        }
+        transform.Translate(dir.normalized*distanceThisFrame, Space.World);
+    }
+    void HitTarget()
+    {
+        GameObject effectIns=(GameObject)Instantiate(impactEffect, transform.position, transform.rotation);
+        Destroy(effectIns, 2f);
+        Destroy(gameObject);
+    }
+    
+}
--- Sickkunts/Assets/Scripts/Turret.cs
+++ Sickkunts/Assets/Scripts/Turret.cs
@@ -5,10 +5,22 @@ using UnityEngine;
 public class Turret : MonoBehaviour
 {
     private Transform target;
+
+
+    [Header("Attributes")]
     public float range=3f;
+    public float fireRate=0.75f;
+    private float fireCountdown=0f;
+
+
+    [Header("Unity Setup Fields")]
     public string enemyTag="Enemy";
     public Transform partToRotate;
     public float turnSpeed=10f;
+
+
+    public GameObject bulletPrefab;
+    public Transform firePoint;
     // Start is called before the first frame update
     void Start()
     {
@@ -50,6 +62,22 @@ public class Turret : MonoBehaviour
         Quaternion lookRotation=Quaternion.LookRotation(dir);
         Vector3 rotation=Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime*turnSpeed).eulerAngles;
         partToRotate.rotation=Quaternion.Euler(0f,rotation.y,0f); 
+
+        if(fireCountdown<=0f)
+        {
+            Shoot();
+            fireCountdown=1f/fireRate;
+        }
+        fireCountdown-=Time.deltaTime;
+    }
+    void Shoot()
+    {   
+       GameObject bulletGO=(GameObject)Instantiate(bulletPrefab,firePoint.position,firePoint.rotation);
+       Bullet bullet = bulletGO.GetComponent<Bullet>();
+       if(bullet!=null)
+       {
+           bullet.Seek(target);
+       }
     }
     void OnDrawGizmosSelected()
     {
